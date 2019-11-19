3082ca151069c162daadd2bbf879e8e29503b0e1
Nethelbert <gilblasse@gmail.com>
2019-11-19 05:31:45 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,11 +10,10 @@ def create_project_hash
   project_list.each_with_object({}) do |project,h|  
       title = project.css("h2 a").text
         h[title] = {
-          :image_link = project.css(".projectphoto-little")[0].values[-2],
-          :description = project.css(".bbcard_blurb").text,
-          :location = project.css(".location-name").text,
-          :percent_funded = project.css(".first.funded").text
+          :image_link => project.css(".projectphoto-little")[0].values[-2],
+          :description => project.css(".bbcard_blurb").text,
+          :location => project.css(".location-name").text,
+          :percent_funded => project.css(".first.funded").text.scan(/\d/).join.to_i
         }
   end
-# binding.pry
 end
